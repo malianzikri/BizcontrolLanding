75496aac1bc3dae7a0dfb60b0a582a07737b5af0
Malian Zikri <malianzikri92@gmail.com>
2026-08-27 19:01:17 +0700

    fixing

---
+++ POS-demo-sales-config-V32.js
@@ -0,0 +1,7 @@
+// V3.2 - GANTI isi demo-sales-config.js di POS dengan ini.
+// CTA dari Demo langsung ke WhatsApp dengan pesan lebih ringan.
+window.BIZCONTROL_DEMO_SALES = {
+  landingUrl: 'https://wa.me/628117199210?text=Halo%20Admin%20BizControl%2C%20saya%20tertarik%20paket%20Rp79rb%2Fbulan.%20Bisa%20dibantu%20info%20aktivasi%3F',
+  monthlyLabel: 'Rp79.000/bulan',
+  pixelId: '1081611444299321'
+};
